24ec71af9e022e32211110d2c028d67946c6ce18
Terry Schmidt <terry.m.schmidt@gmail.com>
2014-05-26 11:48:06 -0500

    Update the awesome_codeblock description

--- lib/author/plugins/awesome_codeblock.rb
+++ lib/author/plugins/awesome_codeblock.rb
@@ -22,21 +22,28 @@
 # You can set the language of the code block. The default is `:text` but you can specify
 # any of the code formats that CodeRay supports.
 #
-#  {% awesome_codeblock lang:"ruby" %}
+#  {% awesome_codeblock lang:ruby %}
 #  code goes here
 #  {% endawesome_codeblock %}
 #
 # You can choose to add line numbers as well. Refer to the documentation for
 # CodeRay::Encoder::HTML for values that can be passed in.
 #
-#  {% awesome_codeblock line_number_format:"inline" %}
+#  {% awesome_codeblock show_line_numbers:true %}
 #  code goes here
 #  {% endawesome_codeblock %}
 #
 # You can choose to highlight specific lines within your code block as well. Just supply
 # a comma separated list of line numbers.
 #
-#  {% awesome_codeblock lines_to_highlight:"2,3,4,7" %}
+#  {% awesome_codeblock highlight:"2,3,4,7" %}
+#  code goes here
+#  ...
+#  {% endawesome_codeblock %}
+#
+# If you want your line numbers to start at a specific number, you can do that too!
+#
+#  {% awesome_codeblock start_at:25 %}
 #  code goes here
 #  ...
 #  {% endawesome_codeblock %}
